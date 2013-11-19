f1375e2640d0bd57ef3a2838ba091fc9cba45f27
JasonBrown <brown23.jason@yahoo.com>
2013-11-18 19:45:47 -0700

    .gitignore update

--- LMS/books.php
+++ LMS/books.php
@@ -9,10 +9,10 @@
         <h1 class="center">Library Book Search</h1>
 
         <?php
-     $db_host = 'localhost';
-    $db_name = 'library_management_system';
-    $db_user = 'root';
-    $db_pass = 'test123';
+        $db_host = 'localhost';
+        $db_name = 'library_management_system';
+        $db_user = 'root';
+        $db_pass = 'test123';
 
         mysql_connect($db_host,$db_user,$db_pass);
         $db_selected = @mysql_select_db($db_name);
@@ -24,7 +24,7 @@
         {
             echo('<!-- Main Input -->');
             echo('<div class="currentrank" style="text-align:right;"><b style="visibility:hidden;font-size:0;">' . rankToString($_SESSION['rank']) . '</b></div>');
-            echo('<input type="text" id="search" autocomplete="off" placeholder="Type search here (EX: Deluxe 120 SAT4K)">');
+            echo('<input type="text" id="search" autocomplete="off" placeholder="Type search here">');
         }
         ?>
     </div>
@@ -34,6 +34,6 @@
     <!-- Show Results -->
     <table id="results-table">
         <h4 id="results">Showing results for: <b id="search-string">Array</b></h4>
-        <ul id="results"></ul>
+        <table id="results"></table>
     </table>
 </div>
